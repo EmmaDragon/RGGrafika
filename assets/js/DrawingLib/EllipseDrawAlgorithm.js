9ff03456f14edd42fb6d5ba3e405ab5b9d13d18e
DESKTOP-FLIMDQP\Emma <emilija>
2020-04-04 12:05:28 +0200

    Elipsa - trigonometrijski algoritam

---
+++ assets/js/DrawingLib/EllipseDrawAlgorithm.js
@@ -0,0 +1,197 @@
+const btnGenerate = document.getElementById("generate");
+const radiusX = document.getElementById("radiusX");
+const radiusY = document.getElementById("radiusY");
+const myCanvas = document.getElementById("myCanvas");
+const algorithm = document.getElementById("selectAlgorithm");
+const width = document.getElementById("width");
+const height = document.getElementById("height");
+const legend = document.getElementById("legend");
+const colorOfEllipse = document.getElementById("colorOfEllipse");
+const btnClearCanvas = document.getElementById("clearCanvas");
+const PI = 3.141592653589;
+let legendOfEllipse=[0,0,0,0];
+
+width.innerHTML+="<strong>"+parseInt(myCanvas.width/4)+"</strong>";
+height.innerHTML+="<strong>"+parseInt(myCanvas.height/4)+"</strong>";
+
+btnGenerate.onclick = (ev) => drawLine(ev);
+algorithm.onchange= (ev) => checkColor(ev);
+btnClearCanvas.onclick = (ev) => clearCanvasElement(ev);
+
+
+
+DrawCoordSystem();
+
+
+
+function clearCanvasElement(ev)
+{
+    var context = myCanvas.getContext('2d');
+    context.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    var w = myCanvas.width;
+    myCanvas.width = 1;
+    myCanvas.width = w;
+    DrawCoordSystem();
+}
+function checkColor(ev)
+{
+    if(algorithm.value=="TA")
+    {
+        if(legendOfEllipse[0]!=0)
+          {
+            colorOfEllipse.value=legendOfEllipse[0];
+            colorOfEllipse.disabled=true;
+          } 
+        else
+            colorOfEllipse.disabled=false;
+            
+         
+    }
+    else if(algorithm.value=="PA")
+    {
+        if(legendOfEllipse[1]!=0)
+          {
+            colorOfEllipse.value=legendOfEllipse[1];
+            colorOfEllipse.disabled=true;
+          } 
+        else
+            colorOfEllipse.disabled=false;
+    }
+    else if(algorithm.value=="DA1")
+    {
+        if(legendOfEllipse[2]!=0)
+          {
+            colorOfEllipse.value=legendOfEllipse[2];
+            colorOfEllipse.disabled=true;
+          } 
+        else
+            colorOfEllipse.disabled=false;
+    }
+    else if(algorithm.value=="DA2")
+    {
+        if(legendOfEllipse[3]!=0)
+          {
+            colorOfEllipse.value=legendOfEllipse[2];
+            colorOfEllipse.disabled=true;
+          } 
+        else
+            colorOfEllipse.disabled=false;
+    }
+    
+}
+function drawLine(ev)
+{
+    if(!validation())
+    {
+        $("#myModal").modal("show");
+    }
+    else
+    {
+        if(algorithm.value == "TA")
+           TA_Algorithm(radiusX.valueAsNumber, radiusY.valueAsNumber);
+            else if(algorithm.value == "PA")
+                    PA_Algorithm(radiusX.valueAsNumber, radiusY.valueAsNumber);
+                else if(algorithm.value == "DA1")
+                        DA1_Algorithm(radiusX.valueAsNumber, radiusY.valueAsNumber);
+                        else if(algorithm.value == "DA2")
+                            DA2_Algorithm(radiusX.valueAsNumber, radiusY.valueAsNumber);
+                        
+    }
+}
+function editLegend(algorithm)
+{
+    const el=document.createElement("div");
+    el.style="color:"+colorOfEllipse.value;
+    el.innerHTML+="<strong>___ "+algorithm+"</strong>";
+    legend.appendChild(el);
+}
+function validation()
+{
+    if(Number.isInteger(radiusX.valueAsNumber) && Number.isInteger(radiusY.valueAsNumber))
+        return true;
+    else
+        return false;
+}
+function TA_Algorithm(a, b)
+{
+    let t, tend, tstep;
+    let dvapi = 2*PI; 
+    let x, y;
+    t = 0.0;
+    tend = PI/2; 
+    tstep = dvapi / (7 * Math.max(a,b));
+    while (t <= tend) 
+    { 
+        x = parseInt(a*Math.cos(t) + 0.5); 
+        y = parseInt(b*Math.sin(t) + 0.5); 
+        Write4Pixel(x, y);
+        t += tstep;
+    } 
+    
+    checkColorStatus(0, "Trigonometrijski algoritam");
+}
+function PA_Algorithm(radius)
+{
+    
+}
+function DA1_Algorithm(radius)
+{
+    
+}
+function DA2_Algorithm(radius)
+{
+    
+}
+
+function checkColorStatus(index, algorithm)
+{
+    if(legendOfEllipse[index]==0)
+    {
+        editLegend(algorithm);
+        legendOfEllipse[index]=colorOfEllipse.value;
+        colorOfEllipse.disabled=true;
+    }
+
+}
+function Write4Pixel(x,y)
+{
+    WritePixel(x,y);
+    WritePixel(-x,y);
+    WritePixel(x,-y);
+    WritePixel(-x,-y);
+
+}
+function WritePixel(x, y)
+{
+    var ctx = myCanvas.getContext("2d");
+    ctx.beginPath();
+    ctx.translate(myCanvas.width/2+(x-1),myCanvas.height/2-(y-1));
+    ctx.rect((x-1),-(y-1),2,2);
+    ctx.fillStyle = colorOfEllipse.value;
+    ctx.fill();
+    ctx.strokeStyle = colorOfEllipse.value;
+    ctx.stroke();
+    ctx.translate(-myCanvas.width/2-(x-1),-myCanvas.height/2+(y-1));
+}
+function DrawCoordSystem()
+{
+    var ctx = myCanvas.getContext("2d");
+    ctx.beginPath();
+    ctx.translate(myCanvas.width / 2, 0);
+    ctx.moveTo(0, 0);
+    ctx.lineTo(0, myCanvas.height);
+    ctx.strokeStyle = '#ff0000';
+    ctx.stroke();
+    ctx.translate(-myCanvas.width / 2, 0);
+    ctx.beginPath();
+    ctx.translate(0, myCanvas.height/2);
+    ctx.moveTo(0, 0);
+    ctx.lineTo(myCanvas.width,0);
+    ctx.strokeStyle = '#00ff00';
+    ctx.stroke();
+    ctx.translate(0, -myCanvas.height/2);
+
+}
+
+
+
