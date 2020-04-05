5a1aaa22b00244a9a285cfd86366b458051ccbdb
DESKTOP-FLIMDQP\Emma <emilija>
2020-04-05 10:58:50 +0200

    Sredjen i deo sa testom

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,35 +1,39 @@
 const btnSumbit=document.getElementById("submition");
 const contentOfPage= document.getElementById("divContent");
+const psDiv = document.getElementById("PS_div");
+const okBtn = document.getElementById("okButton");
 
 
 btnSumbit.onclick = (ev) => checkAnswers(ev);
+okBtn.onclick = (ev) => refreshPage(ev);
 
+function refreshPage(ev)
+{
+    location.reload();
+}
 function checkAnswers(ev)
 {
     let trueAnswers = document.querySelectorAll("input[name='trueAnswer']:checked");
     let points=trueAnswers.length;
     let content = "";
+    let psContent="";
     if(points<5)
     {
-        //tuzan coveculjak
-        content += "<h4 class='subcontent-title' id='quickstart'><span>Rezultati testa znanja</span></h4>";
-        content +="<p style='font-size:large;'>Poštovani, <br> Osvojili ste ukupno <strong> "+points+"</strong> poena.<br> PS: Obratite malo više pažnje na rasterizaciju grafičkih primitivi.</p>"
-
+        content +="<p style='font-size:large;'>Poštovani, <br>Niste baš najbolje savladali predviđeno gradivo. <br> Osvojili ste ukupno <strong style='color:red'> "+points+"/10</strong> poena.</p>"
+        psContent +="PS: Obratite <strong style='color:red'>više</strong> pažnje na rasterizaciju grafičkih primitivi!";
     }
     else if(points == 5)
     {
-        //onako
-        content += "<h4 class='subcontent-title' id='quickstart'><span>Rezultati testa znanja</span></h4>";
-        content +="<p style='font-size:large;'>Poštovani, <br> Osvojili ste ukupno <strong> "+points+"</strong> poena.<br>PS: Nije loše, ali obratite malo više pažnje na rasterizaciju grafičkih primitivi.</p>"
-
+        content +="<p style='font-size:large;'>Poštovani, <br>Solidno ste savladali predviđeno gradivo.<br> Osvojili ste ukupno <strong style='color:orange'> "+points+"/10</strong> poena.</p>";
+        psContent += "PS: Nije loše, ali obratite <strong style='color:orange'>malo više</strong> pažnje na rasterizaciju grafičkih primitivi.";
     }
     else
     {
-        //srecan coveculjak
-        content += "<h4 class='subcontent-title' id='quickstart'><span>Rezultati testa znanja</span></h4>";
-        content +="<p style='font-size:large;'>Poštovani, <br> Osvojili ste ukupno <strong> "+points+"</strong> poena.<br> PS: Odlično ste savladali ovu oblast.</p>"
+        content +="<p style='font-size:large;'>Poštovani, <br>Uspešno ste savladali predviđeno gradivo.<br> Osvojili ste ukupno <strong style='color:#78f542'> "+points+"/10</strong> poena.</p>";
+        psContent +="PS: <strong style='color:#78f542'>Veoma uspešno</strong> ste savladali ovu oblast.";
     }
     contentOfPage.innerHTML =content;
-    
+    psDiv.innerHTML = psContent;
+    $("#myModal").modal('show');
     
 }
